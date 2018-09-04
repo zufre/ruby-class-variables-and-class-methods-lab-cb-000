9b4cd53a5735d77464ea98692b698f0f69261aee
Cedric David <cedric.david@btopenworld.com>
2018-09-04 00:37:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
 
   @@count = 0
+  @@genres = []
 
   attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
@@ -8,10 +9,11 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres < genre
 
   end
   def self.count
     @@count
   end
-    
+
 end
