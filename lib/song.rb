bd3be3712acbaabfa528779b566128fa402129b7
Cedric David <cedric.david@btopenworld.com>
2018-09-04 01:10:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,43 @@
 class Song
-  attr_accessor :name, :artist, :genre
+
   @@count = 0
   @@genres = []
   @@artists = []
+
+  attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << @genre
-    @@artists << @artist
+    @@genres << genre
+    @@artists << artist
+
   end
   def self.count
     @@count
+  end
+  def self.genre_count
+    hash = {}
+    @@genres.each do |genre|
+      if hash[genre] == nil
+        hash[genre] = 1
+      else
+        hash[genre] += 1
+      end
+    end
+    hash
+  end
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      if !hash.include?  artist
+        hash[artist] = 1
+      else
+        hash[artist] += 1
+      end
+    end
+    hash
   end
   def self.artists
     array = []
@@ -24,7 +49,7 @@ class Song
     array
   end
   def self.genres
-    array = []
+    array =[]
     @@genres.each do |genre|
       if !array.include? genre
         array << genre
@@ -32,26 +57,5 @@ class Song
     end
     array
   end
-  def self.genre_count
-    hash = {}
-    @@genres.each do |genre|
-      if !hash.include? genre
-        hash[genre] = 1
-      else
-        hash[genre] += 1
-      end
-    end
-    hash
-  end
-  def self.artist_count
-    hash = {}
-    @@artists.each do |artist|
-      if !hash.include? artist
-        hash[artist] = 1
-      else
-        hash[artist] += 1
-      end
-    end
-    hash
-  end
+
 end
